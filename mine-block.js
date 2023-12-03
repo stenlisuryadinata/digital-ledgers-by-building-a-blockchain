348590c104dc3be0e3005029c77fdea480d1697c
Stenli Suryadinata <47641964+stenlisuryadinata@users.noreply.github.com>
2023-12-03 14:44:08 +0700

    update add-block.js
    update add-block.js
    
    -rename add-block.js to mine-block.js
    -add new dependencies
    npm install crypto-js
    -import sha256 from 'crypto-js/sha256.js'; to turned the password into a 256 bit hash

--- mine-block.js
+++ mine-block.js
@@ -1,8 +1,11 @@
 import { getBlockchain, writeBlockchain, getTransactions, writeTransactions } from './blockchain-helpers.js';
+import sha256 from 'crypto-js/sha256.js';
 
 const blockchain = getBlockchain();
 const previousBlock = blockchain[blockchain.length - 1];
 const transactions = getTransactions();
+const hash = sha256('passwords').toString();
+console.log(hash);
 
 const newBlock = {
   hash: Math.random().toString(),
