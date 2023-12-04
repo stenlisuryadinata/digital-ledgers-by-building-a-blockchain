3b1c8b9a745152c2aeac12354951309729869b39
Stenli Suryadinata <47641964+stenlisuryadinata@users.noreply.github.com>
2023-12-05 03:43:17 +0700

    update mine-block.js
    update mine-block.js
    
    -Create a while loop that checks if !hash.startsWith('0').

--- mine-block.js
+++ mine-block.js
@@ -11,7 +11,21 @@ let nonce = 0;
 // const hash = sha256(previousBlock.hash + JSON.stringify(transactions)).toString(); 
 // nonce is added as the first part of the string used to create the hash
 let hash = sha256(nonce + previousBlock.hash + JSON.stringify(transactions)).toString(); 
-console.log(hash);
+//the hash to start with a zero. Create a while loop that checks if !hash.startsWith('0').
+//If your initial hash starts with zero, you will not enter this loop. If it doesn't, you want to look for a new hash until it does.
+while (!hash.startsWith('0')) { 
+  nonce++;
+  hash = sha256(nonce + previousBlock.hash + JSON.stringify(transactions)).toString();
+  console.log(`nonce = ${nonce}`);
+  //console.log(hash);
+  //log that prints the hash in the same fashion.
+  //The last line of your terminal output should print hash = <hash>, with <hash> being a hash that starts with one zero
+  //to run node mine-block.js in the terminal to see the hash
+  console.log(`hash = ${hash}`)
+}
+
+//It went through your loop until it found a hash that starts with zero
+
 
 const newBlock = {
   //hash: Math.random().toString(),
